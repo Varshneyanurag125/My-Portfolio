e9d211ba4c9e4e60ce9af897f075db2216c2b070
Varshneyanurag125 <22bee031@nith.ac.in>
2024-09-09 23:13:32 +0530

    REMOVE ERROR

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -30,10 +30,10 @@ const Footer: React.FC = () => {
             <div className=' flex mt-16 md:flex-row flex-col justify-between items-center'>
                 <p className=' md:text-base text-sm md:font-normal font-light ' >Copyright © 2024 Anurag Varshney</p>
                 <div className=' flex items-center md:gap-3 gap-6'>
-                    {socialMedia.map((profile) => (
-                        <div key={profile.id} className=' w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg
+                    {socialMedia.map((socialMedia) => (
+                        <div key={socialMedia.id} className=' w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg
                          border-black-300'>
-                            <Image src={profile.img} alt="" width="20" height="20" />
+                            <Image src={socialMedia.img} alt="" width="20" height="20" />
                         </div>
                     ))}
                 </div>
