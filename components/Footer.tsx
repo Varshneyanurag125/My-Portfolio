c065a41f7fd8fd9bc0f1a5d787bc39b6bf2933f3
Varshneyanurag125 <22bee031@nith.ac.in>
2024-09-09 23:08:24 +0530

    REMOVE ERROR

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 import MagicButton from './ui/MagicButton';
 import { FaLocationArrow } from 'react-icons/fa';
 import { socialMedia } from '@/data';
@@ -32,7 +33,7 @@ const Footer: React.FC = () => {
                     {socialMedia.map((profile) => (
                         <div key={profile.id} className=' w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg
                          border-black-300'>
-                            <img src={profile.img} alt={profile.id} width={20} height={20} />
+                            <Image src={profile.img} alt="" width="20" height="20" />
                         </div>
                     ))}
                 </div>
